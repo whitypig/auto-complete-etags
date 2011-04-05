19f360632300134a8a07c3142ca22ecfc38c5ac2
whitypig <whitypig@gmail.com>
2011-04-05 16:07:05 +0900

    Added tag files and empty test file.

---
+++ test.c
@@ -0,0 +1,23 @@
+void simple_func(void)
+{
+}
+
+void simple_func2(int a,
+                 int b)
+{
+  return;
+}
+
+int
+multiple_line_func(void)
+{
+  return 1;
+}
+
+const char *
+multiple_line_va_arg_func(int a,
+                          int b,
+                          ...)
+{
+  return NULL;
+}
---
+++ test.el
@@ -0,0 +1,2 @@
+(require 'auto-complete-etags)
+
