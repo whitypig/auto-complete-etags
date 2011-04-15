58718d676f2367ad32231223bd09805689a2a81b
whitypig <whitypig@gmail.com>
2011-04-15 17:30:39 +0900

    Removed and corrected some codes.

--- test.el
+++ test.el
@@ -2,11 +2,11 @@
 (require 'el-expectations)
 
 ;; ac-etags-search-for-signature
-(defun test-ac-etags-search-for-signature (item)
+(defun test-ac-etags-search-for-documentation (item)
   (let ((ret nil) (major-mode 'c-mode) (org-name tags-file-name) (org-list tags-table-list)
         (tagfile (expand-file-name "./TAGS")))
     (setq tags-table-list `(,tagfile))
-    (setq ret (ac-etags-search-for-signature item))
+    (setq ret (ac-etags-search-for-documentation item))
     (setq tags-file-name org-name)
     (setq tags-table-list org-list)
     ret))
@@ -16,50 +16,19 @@
 ;; Tests for ac-etags-search-for-signature
 (expectations
   (desc "Test for signature of a func-decl on single line")
-  (expect "void simple_func(void)" (test-ac-etags-search-for-signature "simple_func"))
+  (expect "void simple_func(void)" (test-ac-etags-search-for-documentation "simple_func"))
 
   (desc "Test for signature of a func-decl with multiple args")
-  (expect "void simple_func2(int a, int b)" (test-ac-etags-search-for-signature "simple_func2"))
+  (expect "void simple_func2(int a, int b)" (test-ac-etags-search-for-documentation "simple_func2"))
 
   (desc "Test for signature on multiple lines")
-  (expect "int multiple_line_func(void)" (test-ac-etags-search-for-signature "multiple_line_func"))
+  (expect "int multiple_line_func(void)" (test-ac-etags-search-for-documentation "multiple_line_func"))
 
   (desc "Test for signature of multiple args on multiple lines")
   (expect "const char* multiple_line_va_arg_func(int a, int b, ...)"
-    (test-ac-etags-search-for-signature "multiple_line_va_arg_func"))
+    (test-ac-etags-search-for-documentation "multiple_line_va_arg_func"))
 
   (desc "No documentation found")
   (expect "No documentation found."
-    (test-ac-etags-search-for-signature "foo"))
-  )
-
-;; Tests for ac-etags-is-function
-(expectations
-  (expect (non-nil) (ac-etags-is-function-maybe "f" "f(void)"))
-  (expect (non-nil) (ac-etags-is-function-maybe "f" "f()"))
-  (expect (non-nil) (ac-etags-is-function-maybe "f" "f() {"))
-  (expect (non-nil) (ac-etags-is-function-maybe "f" "f(int a, const char *p, ...)"))
-
-  (desc "Maybe a function")
-  (expect (non-nil) (ac-etags-is-function-maybe "f" "f(int a,"))
-  (expect nil (ac-etags-is-function-maybe "mcr" "#define mcr(e) #(e)"))
-  (expect nil (ac-etags-is-function-maybe "g" "int g(void)\\")))
-
-(expectations
-  (desc "Test for signature of a func-decl on single line")
-  (expect "void simple_func(void)" (test-ac-etags-get-c-mode-document "simple_func"))
-
-  (desc "Test for signature of a func-decl with multiple args")
-  (expect "void simple_func2(int a, int b)" (test-ac-etags-get-c-mode-document "simple_func2"))
-
-  (desc "Test for signature on multiple lines")
-  (expect "int multiple_line_func(void)" (test-ac-etags-get-c-mode-document "multiple_line_func"))
-
-  (desc "Test for signature of multiple args on multiple lines")
-  (expect "const char* multiple_line_va_arg_func(int a, int b, ...)"
-    (test-ac-etags-get-c-mode-document "multiple_line_va_arg_func"))
-
-  (desc "No documentation found")
-  (expect (error)
-    (test-ac-etags-get-c-mode-document "foo"))
+    (test-ac-etags-search-for-documentation "foo"))
   )
