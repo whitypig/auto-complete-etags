054de3e466b64683a9cb081c0fe44de75bdcbade
whitypig <whitypig@gmail.com>
2011-04-17 04:15:55 +0900

    Did refactoring test-ac-etags-search-for-documentation.

--- test.el
+++ test.el
@@ -2,39 +2,40 @@
 (require 'el-expectations)
 
 ;; ac-etags-search-for-signature
-(defun test-ac-etags-search-for-documentation (item)
+(defun test-ac-etags-search-for-documentation (item tagfile)
   (let ((ret nil) (major-mode 'c-mode) (org-name tags-file-name) (org-list tags-table-list)
-        (tagfile (expand-file-name "./c.TAGS")))
+        (tagfile (expand-file-name tagfile)))
     (setq tags-table-list `(,tagfile))
     (setq ret (ac-etags-search-for-documentation item))
     (setq tags-file-name org-name)
     (setq tags-table-list org-list)
     ret))
 
-;; Synopsis: (expect expected actual)
-
 ;; Tests for ac-etags-search-for-signature
 (expectations
   (desc "Test for signature of a func-decl on single line")
-  (expect "void simple_func(void)" (test-ac-etags-search-for-documentation "simple_func"))
+  (expect "void simple_func(void)" (test-ac-etags-search-for-documentation "simple_func" "c.TAGS"))
 
   (desc "Test for signature of a func-decl with multiple args")
-  (expect "void simple_func2(int a, int b)" (test-ac-etags-search-for-documentation "simple_func2"))
+  (expect "void simple_func2(int a, int b)" (test-ac-etags-search-for-documentation "simple_func2"
+                                                                                    "c.TAGS"))
 
   (desc "Test for signature on multiple lines")
-  (expect "int multiple_line_func(void)" (test-ac-etags-search-for-documentation "multiple_line_func"))
+  (expect "int multiple_line_func(void)" (test-ac-etags-search-for-documentation "multiple_line_func"
+                                                                                 "c.TAGS"))
 
   (desc "Test for signature of multiple args on multiple lines")
   (expect "const char* multiple_line_va_arg_func(int a, int b, ...)"
-    (test-ac-etags-search-for-documentation "multiple_line_va_arg_func"))
+    (test-ac-etags-search-for-documentation "multiple_line_va_arg_func"
+                                            "c.TAGS"))
 
   (desc "Test for a function with macro")
   (expect "MACRO1 MACRO2 const char * macro_func(int a)"
-    (test-ac-etags-search-for-documentation "macro_func"))
+    (test-ac-etags-search-for-documentation "macro_func" "c.TAGS"))
 
   (desc "No documentation found")
   (expect "No documentation found."
-    (test-ac-etags-search-for-documentation "foo"))
+    (test-ac-etags-search-for-documentation "foo" "c.TAGS"))
 
   ;; For now, we ignore old-style functions.
   ;; (desc "Test for old-style function declaration")
